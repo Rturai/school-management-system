381d8cb3a65017e7e70b0a5250b30ae634cf88ab
Ritu Rai <123947180+copms@users.noreply.github.com>
2026-06-20 10:51:24 +0530

    fix: server running successfully without path error

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,7 @@ const app = express();
 
 // ✅ CORS Problem ko permanently khatam karne ke liye bilkul clean wildcard setup
 app.use(cors({ origin: '*' }));
-app.options('*', cors());
+
 
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
